1d4ee5241fbdc951b4eba5f6eb6806f89d1351bd
Robson Rocha <robsonrccha@gmail.com>
2025-03-01 00:58:54 -0300

    Update 7

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// Servir arquivos estáticos da pasta "public"
+app.use(express.static(path.join(__dirname, "public")));
 
 // Conectar ao MongoDB Atlas
 mongoose.connect("mongodb+srv://rrocha:P1hXmR84JwrZRIUK@cluster0.hbxfupw.mongodb.net/ecomapDB", {
@@ -40,9 +44,13 @@ app.post("/subscribe", async (req, res) => {
   }
 });
 
+// Servir a landing page no acesso a "/"
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 // Se o Vercel fornecer o PORT, usamos ele, senão usamos a porta 5000.
-const port = process.env.PORT || 5000;  
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
-
